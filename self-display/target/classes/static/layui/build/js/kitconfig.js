6fdb96f13fe71500c788a5ac30d52c4381c2b199
xiaofeiyayaya <32194187+xiaofeiyayaya@users.noreply.github.com>
2018-11-05 10:04:48 +0800

    删除target

--- self-display/target/classes/static/layui/build/js/kitconfig.js
+++
@@ -1,9 +0,0 @@
-
- ;layui.define(function(exports) {
-
-    var kitconfig = {
-        resourcePath: '/layui/build', //框架资源路径-相对路径和绝对路径
-    };
-
-    exports('kitconfig', kitconfig);
-});
--- self-display/target/self-display-0.0.1-SNAPSHOT/WEB-INF/pages/common/tld.jsp
+++
@@ -1,2 +0,0 @@
-<%@ taglib uri="/WEB-INF/tld/cm.tld" prefix="cm" %>
-<%@ taglib prefix="tags" tagdir="/WEB-INF/tags" %>
